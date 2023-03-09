fdb958f0469a76e39b5cbdc2148e589536e507a0
alisonvmx <alisonmuniz.am@gmail.com>
2023-03-09 10:48:51 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+//import { useCallback, useEffect, useState } from "react";
+//importe dos componentes
+//import game from "./components/screen_game";
+//import result from "./components/screen_result";
+
+
+//importe dos dados mocados
+import { wordsList } from './components/data/words';
+
+//importe do css
+import { useState } from 'react';
 import './App.css';
+//import screenMain from './components/screenMain';
+
+const stages =[
+  {id: 1, name:"main"},
+  {id: 2, name:"game"},
+  {id: 3, name:"result"}
+
+]
 
 function App() {
+  const [gameStage,setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
+
+  console.log(words);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ src/components/screenMain.js
@@ -0,0 +1,13 @@
+import "./screenMain.css";
+
+const screenMain = () => {
+  return (
+    <div className="main">
+        <h1>Secret Word</h1>
+        <p>clique no botão abaixo para iniciar a partida</p>
+        {/* <button onClick={startgame}>start</button> */}
+    </div>
+  )
+}
+
+export default screenMain
